246520585ef2f4131369cce8522cbe676a33fce3
Sinclert <Sinclert@hotmail.com>
2018-12-13 20:19:43 +0100

    Fixing 'write_to_file' func and '#pragma schedule'

--- gs_openmp.c
+++ gs_openmp.c
@@ -43,20 +43,20 @@ void allocate_matrix(float **mat, int n, int m){
 
 
 // Write the time results into a CSV file
-void write_to_file(int n, int num_blocks, int num_threads, float total_time, float exec_time) {
+void write_to_file(int n, char *schedule_type, float total_time, float exec_time) {
 
 	FILE *f;
 	char* file_name = "results.csv";
 
 	if (access(file_name, F_OK) == -1) {
  		f = fopen(file_name, "a");
-		fprintf(f, "Matrix size;Blocks;Threads per block;Total time;Operations time;\n");
+		fprintf(f, "Matrix size;Schedule type;Total time;Operations time;\n");
 	}
 	else {
 		f = fopen(file_name, "a");
 	}
 
-	fprintf(f, "%d;%d;%d;%f;%f;\n", n, num_blocks, num_threads, total_time, exec_time);
+	fprintf(f, "%d;%s;%f;%f;\n", n, schedule_type, total_time, exec_time);
 	fclose(f);
 }
 
@@ -64,7 +64,7 @@ void write_to_file(int n, int num_blocks, int num_threads, float total_time, flo
 
 
 // Solves the matrix splitting the rows into different threads
-void solver(float **mat, int n, int m) {
+void solver(float **mat, int n, int m, int max_cells) {
 
 	float temp;
 	float diff = 0;
@@ -73,7 +73,7 @@ void solver(float **mat, int n, int m) {
 	int cnt_iter = 0;
 
 	const int mat_dim = n*n;
-	const int max_cell = (n*m) - (2*n);
+	const int max_pos = (n*m) - (2*n);
 
 
   	while (!done && (cnt_iter < MAX_ITER)) {
@@ -81,8 +81,8 @@ void solver(float **mat, int n, int m) {
 
   		// Neither the first row nor the last row are solved
   		// (that's why it starts at "n" and it goes up to "num_elems - 2n")
-		#pragma omp parallel for private(i, temp) reduction (+:diff) 				// TODO: Choose static | guided | dynamic 
-  		for (int i = n; i < max_cell; i++) {
+		#pragma omp parallel for schedule(static, max_cells) private(i, temp) reduction(+:diff) 
+  		for (int i = n; i < max_pos; i++) {
 
   			// Additionally, neither the first nor last column are solved
   			// (that's why the first and last positions of "rows" are skipped)
@@ -100,7 +100,7 @@ void solver(float **mat, int n, int m) {
 			diff += abs((*mat)[i] - temp);
       	}
 
-		#pragma omp barrier															// TODO: Necessary?
+		#pragma omp barrier	// TODO: Necessary?
 
 		if (diff/mat_dim < TOL) {
 			done = 1;
@@ -118,7 +118,7 @@ int main(int argc, char *argv[]) {
 
 	if (argc < 2) {
     	printf("Call this program with two parameters: matrix_size communication \n");
-    	printf("\t matrix_size: Add 2 to a power of 2 (e.g. : 18, 1026)\n");				// TODO: Put more arguments
+    	printf("\t matrix_size: Add 2 to a power of 2 (e.g. : 18, 1026)\n");
     	exit(1);
 	}
 
@@ -139,7 +139,7 @@ int main(int argc, char *argv[]) {
 	clock_t i_exec_t = clock();
 
 	// Parallelized solver
-	solver(&a, n, n);
+	solver(&a, n, n, max_cells);
 
 	// Final operation time
 	clock_t f_exec_t = clock();
@@ -155,5 +155,5 @@ int main(int argc, char *argv[]) {
 	printf("Total time: %f\n", total_time);
 	printf("Operations time: %f\n", exec_time);
 
-	write_to_file(n, num_blocks, threads_per_block, total_time, exec_time);			// TODO: Change arguments
+	write_to_file(n, "static", total_time, exec_time);
 }
