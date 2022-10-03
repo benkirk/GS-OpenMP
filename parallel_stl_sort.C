bb69a5163a0b60b34fd0957bcfa377f7386c03f1
Ben Kirk <benkirk@ucar.edu>
2022-10-03 14:05:52 -0600

    error checking vs. seq truth

--- parallel_stl_sort.C
+++ parallel_stl_sort.C
@@ -29,59 +29,65 @@ void getExecutionTime(const std::string& title, Func func){                   //
 
 
 template <typename T>
-void print_front(const std::vector<T> &v)
+void print_front(const std::vector<T> &v, const std::string &label="")
 {
-  std::cout << "v.size()=" << v.size() << " : ";
+  std::cout << label << "v.size()=" << v.size() << "; ";
   for (auto i=0; i!=11 && i<v.size(); ++i)
     std::cout << v[i] << " ";
-  std::cout << std::endl;
+  std::cout << "...\n";
 }
 
 
 
 int main(){
 
-
   typedef unsigned int size_type;
   std::vector<size_type> randValues, workVec;
   randValues.reserve(size); /**/ workVec.reserve(size);
+
+  auto seq_size=randValues.size();
 
   std::mt19937 engine;
   std::uniform_int_distribution<size_type> uniformDist(0, std::numeric_limits<size_type>::max());
   for (auto i = 0 ; i < size ; ++i) randValues.push_back(uniformDist(engine));
 
-  getExecutionTime("std::copy() / std::sort() / std::unique() / std::execution::seq", [ &randValues, &workVec]() {
+
+  getExecutionTime("std::copy() / std::sort() / std::unique() / std::execution::seq", [ &randValues, &workVec, &seq_size]() mutable {
       auto policy =  std::execution::seq;
       workVec.resize(randValues.size());
       std::copy(policy, randValues.begin(), randValues.end(), workVec.begin());
-      print_front(workVec);
+      print_front(workVec, "input: ");
       std::sort(policy, workVec.begin(), workVec.end());
+      print_front(workVec, "after sort: ");
       auto new_end = std::unique(policy, workVec.begin(), workVec.end());
       workVec.erase(new_end, workVec.end());
-      print_front(workVec);
-    }); std::cout << '\n';
+      seq_size = workVec.size();
+      print_front(workVec, "after unique: ");
+    }); print_front(workVec, "final: "); std::cout << '\n';
 
-  getExecutionTime("std::copy() / std::sort() / std::unique() / std::execution::par", [ &randValues, &workVec]() {
+  getExecutionTime("std::copy() / std::sort() / std::unique() / std::execution::par", [ &randValues, &workVec, seq_size]() mutable {
       auto policy =  std::execution::par;
       workVec.resize(randValues.size());
       std::copy(policy, randValues.begin(), randValues.end(), workVec.begin());
-      print_front(workVec);
+      print_front(workVec, "input: ");
       std::sort(policy, workVec.begin(), workVec.end());
+      print_front(workVec, "after sort: ");
       auto new_end = std::unique(policy, workVec.begin(), workVec.end());
       workVec.erase(new_end, workVec.end());
-      print_front(workVec);
-    }); std::cout << '\n';
+      print_front(workVec, "after unique: ");
+      if (workVec.size() != seq_size) std::cerr << "==> ERROR: size mismatch from serial algorithm!\n";
+    }); print_front(workVec, "final: "); std::cout << '\n';
 
-  getExecutionTime("std::copy() / std::sort() / std::unique() / std::execution::par_unseq", [ &randValues, &workVec]() {
+  getExecutionTime("std::copy() / std::sort() / std::unique() / std::execution::par_unseq", [ &randValues, &workVec, seq_size]() mutable {
       auto policy =  std::execution::par_unseq;
       workVec.resize(randValues.size());
       std::copy(policy, randValues.begin(), randValues.end(), workVec.begin());
-      print_front(workVec);
+      print_front(workVec, "input: ");
       std::sort(policy, workVec.begin(), workVec.end());
+      print_front(workVec, "after sort: ");
       auto new_end = std::unique(policy, workVec.begin(), workVec.end());
       workVec.erase(new_end, workVec.end());
-      print_front(workVec);
-    });
-
-  std::cout << '\n';
+      print_front(workVec, "after unique: ");
+      if (workVec.size() != seq_size) std::cerr << "==> ERROR: size mismatch from serial algorithm!\n";
+    }); print_front(workVec, "final: "); std::cout << '\n';
 }
